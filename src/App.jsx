21829e86b746deddaef8e1a14e34efd16d08346a
DurgaKC <kcdurga732@gmail.com>
2025-12-07 22:19:35 +0545

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './Weather'
+
+const App = () => {
+  return (
+       <div className="min-h-screen grid place-items-center bg-[#e2d4ff]">
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
